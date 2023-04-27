4cf3a015fa6cab54f4884983b5d2e366387f7f55
Vivek Tiwari <102945885+Vivek-Tiwari20@users.noreply.github.com>
2023-04-27 22:51:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function getFormvalue() {
-    //Write your code here
+ let fName = document.getElementById("fname");
+ let lName = document.getElementById("lname");
 
-}
+ let form = document.getElementById("form1");
+
+ let submit = document.getElementById("submit");
+
+ submit.addEventListener("click",btnfun);
+
+ function btnfun(){
+        fName = document.getElementById("fname").value;
+      lName = document.getElementById("lname").value;
+
+     let firstName = document.createElement("p");
+      firstName.innerText = fName;
+
+      let lastName = document.createElement("p");
+      lastName.innerText = lName;
+
+      alert(fName+" "+lName)
+ }
